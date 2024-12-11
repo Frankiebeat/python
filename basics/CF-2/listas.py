487a9657870ff139233027701c2b3ed0c1cc22f0
franciscoboll <franciscoboll-externo@gmail.com>
2024-12-11 16:09:45 -0300

    added basics dir

---
+++ basics/CF-2/listas.py
@@ -0,0 +1,23 @@
+# Las listas son un tipo de dato que nos permite almacenar varios valores en una sola variable.
+# Las listas se pueden definir con corchetes [] o con la función list()
+# Las listas pueden contener cualquier tipo de dato.
+# Las listas pueden contener cualquier cantidad de elementos.
+# Las listas pueden contener elementos repetidos.
+# Las listas pueden ser modificadas en tiempo de ejecución
+# Las listas pueden ser ordenadas o invertidas
+# Las listas pueden ser anidadas
+# Las listas pueden ser utilizadas como pilas o colas
+
+# Crear una lista
+miLista = [1, 2, 3, 4, 5]
+
+# Crear una lista con la función list()
+miLista = list((1, 2, 3, 4, 5))
+
+# Crear una lista con diferentes tipos de datos
+miLista = [1, 2.5, "Hola", True]
+
+# Crear una lista vacía
+miLista = []
+
+# Los indices en las listas comienzan en 0
